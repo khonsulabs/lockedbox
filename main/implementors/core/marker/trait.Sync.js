487d0abc7cd373463641c4bf914918a1ecfaf753
kl-botsu <botsu@khonsulabs.com>
2023-09-12 18:32:40 +0000

    Deploying to gh-pages from @ khonsulabs/lockedbox@23857493dfe1d88964fd9ab42b31c3e4930ac5ac 🚀

--- main/implementors/core/marker/trait.Sync.js
+++ main/implementors/core/marker/trait.Sync.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"lockedbox":[["impl&lt;T&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"lockedbox/struct.LockedBox.html\" title=\"struct lockedbox::LockedBox\">LockedBox</a>&lt;T&gt;",1,["lockedbox::LockedBox"]]]
+"lockedbox":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"lockedbox/struct.LockedBox.html\" title=\"struct lockedbox::LockedBox\">LockedBox</a>&lt;T&gt;<span class=\"where fmt-newline\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a>,</span>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
